e64f5b0d157ce967a718967cf28f869c8b1bf71a
hareesh <hareesh@reldyn.co>
2025-08-13 22:12:22 +0530

    Added input compoent

--- apps/docs/app/docs/layout.tsx
+++ apps/docs/app/docs/layout.tsx
@@ -7,7 +7,7 @@ import { RootProvider } from "fumadocs-ui/provider";
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <div className="">
-      <RootProvider dir="rtl">
+      <RootProvider dir="ltr">
         <DocsLayout tree={source.pageTree} {...baseOptions}>
           {children}
         </DocsLayout>
